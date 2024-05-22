5f9e15a82ce61ec7d532c9e569b7201d97e29259
Lorram Marques <gabriellorranaraujomarques@gmail.com>
2024-05-22 10:25:36 -0300

    LikeLog DELETE endpoint removed

--- backend/src/main/java/com/lorram/social/controllers/LikeController.java
+++ backend/src/main/java/com/lorram/social/controllers/LikeController.java
@@ -4,7 +4,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -30,11 +29,5 @@ public class LikeController {
 	public ResponseEntity<LikeLogDTO> findById(@PathVariable Long id) {
 		LikeLogDTO dto = service.findById(id);
 		return ResponseEntity.ok().body(dto);
-	}
-	
-	@DeleteMapping(value = "/{id}")
-	public ResponseEntity<Void> delete(@PathVariable Long id) {
-		service.delete(id);
-		return ResponseEntity.noContent().build();
 	}
 }
